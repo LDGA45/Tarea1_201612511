d8e6f7cc35b947106d10217c988721bf7b351f82
LDGA45 <36612290+LDGA45@users.noreply.github.com>
2023-08-05 21:12:22 -0600

    ADD endpoint sumar
    Se crea el endpoint para sumar dos numeros

---
+++ api/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000; // Puedes cambiar el puerto si es necesario
+
+app.use(express.json()); // Agregar middleware para analizar el cuerpo JSON de las solicitudes POST
+
+// Ruta para sumar dos valores (método POST)
+app.post('/sumar', (req, res) => {
+  const { num1, num2 } = req.body;
+  const resultado = parseFloat(num1) + parseFloat(num2);
+  res.json({ resultado });
+});
+
+// Ruta para mostrar el carnet (método GET)
+app.get('/carnet', (req, res) => {
+  res.send('carnet: 201612511');
+});
